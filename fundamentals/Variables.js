a8fa96b7c39596ce69dc7037fda2addd58e072a0
italomezquita <italomezquita@gmail.com>
2026-01-31 13:04:36 -0300

    Creating Variables.js

---
+++ fundamentals/Variables.js
@@ -0,0 +1,13 @@
+// Variáveis em JavaScript
+let name = "Italo";
+let age = 18;
+
+console.log("My name is", name, "and I am", age, "years old");
+
+
+// Variáveis em JavaScript
+let name = "Italo";
+let age = 18;
+
+console.log("Name:", name);
+console.log("Age:", age);
